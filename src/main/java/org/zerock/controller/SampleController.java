fecbbbaa05ada71e523bc20cd67dca9ed4fa648a
kimjuhyeon <tmxhsk99@naver.com>
2018-11-13 16:19:14 +0900

    first commit

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,11 @@
+package org.zerock.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+
+}
